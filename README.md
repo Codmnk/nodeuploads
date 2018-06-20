a8dcbe72df24d8d4a8c744e6024f1c17b196f88b
Prem Acharya <prem.acharya@hotmail.com>
2018-06-20 23:02:39 +1000

    Multiple upload function added

--- README.md
+++ README.md
@@ -23,6 +23,16 @@ $ npm start
 ```
 
 ## App Info
+change number of images allowed to upload by changing number in app.js line 22
+```
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 25000000},
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb)
+    }
+}).array('myfile', 5) // 5 indicates that the number of file allowed to upload at once.
+```
 
 ### Author
 
--- app.js
+++ app.js
@@ -1,73 +1,82 @@
-const express = require('express');
-const multer = require('multer');
-const ejs = require('ejs');
-const path = require('path');
+const express = require('express')
+const multer = require('multer')
+const ejs = require('ejs')
+const path = require('path')
 
-// Set The Storage Engine
+
+// Set storage Engine
 const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
 
-// Init Upload
+// Init upload
 const upload = multer({
-  storage: storage,
-  limits:{fileSize: 1000000},
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-}).single('myImage');
+    storage: storage,
+    limits: {fileSize: 25000000},
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb)
+    }
+}).array('myfile', 5)
 
-// Check File Type
 function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
+    // allow files
+    const fileTypes = /jpeg|jpg|png|gif|mp4/
 
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
+    // check file types
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+
+    // check mime
+    const mimetype = fileTypes.test(file.mimetype)
+
+    if(mimetype && extname){
+        return cb(null, true)
+    }else{
+        return cb('Err: Image onlY!')
+    }
 }
 
 // Init app
-const app = express();
+const app = express()
+//ejs
+app.set('view engine', 'ejs')
 
-// EJS
-app.set('view engine', 'ejs');
 
-// Public Folder
-app.use(express.static('./public'));
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
-app.get('/', (req, res) => res.render('index'));
+app.post('/upload', (req, res)=>{
+    upload(req, res, (err =>{
+        if(err){
+            // console.log(req.files)
+            res.render('index', {
+                msg: err
+            })
+        }else{ 
+            if(req.files == undefined){
+                res.render('index', {
+                    msg: 'No File Selected!'
+                })
+            }else{ 
+                console.log(req.files)
+                res.render('index', {
+                    msg: 'Image uploaded',
+                    file: `uploads/${req.files.filename}`
+                }
+                )
+            }
+        }
+    }))
+ 
+})
+// public folder
+app.use(express.static('./public'))
+const port = 3000
 
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
-        msg: err
-      });
-    } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        res.render('index', {
-          msg: 'File Uploaded!',
-          file: `uploads/${req.file.filename}`
-        });
-      }
-    }
-  });
-});
 
-const port = 3000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.listen(port, ()=> console.log(`Server running on ${port}`))
